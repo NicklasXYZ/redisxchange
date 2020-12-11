c07c78370b6e32fb7ae686cf5e27774ec8d62995
NicklasXYZ <nsa200293@live.dk>
2020-12-11 13:37:05 +0100

    Add additional library dependencies

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
-from setuptools import setup
+from setuptools import (
+    setup,
+    find_packages,
+)
 
 setup(
     name = "redisxchange",
     url = "https://github.com/nicklasxyz/redisxchange",
     author = "Nicklas Sindlev Andersen",
-    packages = ["redisxchange"],
+    packages = find_packages(include=["redisxchange"]),
     include_package_data = True,
-    install_requires = ["msgpack", "redis"],
+    install_requires = ["msgpack", "redis", "asgiref"],
     version = "0.1",
     license = "MIT",
     description = "",
